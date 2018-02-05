ce3f35a48bdeb404af75e822828f4b17baa8e083
Alexander Kallaway <kallaway@me.com>
2018-02-05 00:03:58 -0500

    first part done - quiz works

--- 01-quiz/main.go
+++ 01-quiz/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -27,9 +28,18 @@ func main() {
 
 	problems := parseLines(lines)
 
+	correct := 0
 	for i, p := range problems {
 		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+
+		if answer == p.a {
+			correct++
+		}
 	}
+
+	fmt.Printf("You scored %d out of %d.", correct, len(problems))
 }
 
 func parseLines(lines [][]string) []problem {
@@ -37,7 +47,7 @@ func parseLines(lines [][]string) []problem {
 	for i, line := range lines {
 		ret[i] = problem{
 			q: line[0],
-			a: line[1],
+			a: strings.TrimSpace(line[1]),
 		}
 	}
 
