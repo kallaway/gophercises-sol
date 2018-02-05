c4de16410751bf04558ca381d9213572fa683373
Alexander Kallaway <kallaway@me.com>
2018-02-04 23:53:04 -0500

    working through first gophersize (after having done my version)

---
+++ 01-quiz/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	csvFilename := flag.String("csv", "problems.csv", "csv file in format 'question,answer' on each line")
+	flag.Parse()
+
+	file, err := os.Open(*csvFilename)
+
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open a CSV file: %s\n", *csvFilename))
+	}
+
+	r := csv.NewReader(file)
+
+	lines, err := r.ReadAll()
+
+	if err != nil {
+		exit("Failed to parse the provided CSV file")
+	}
+
+	problems := parseLines(lines)
+
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
+	}
+}
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+	for i, line := range lines {
+		ret[i] = problem{
+			q: line[0],
+			a: line[1],
+		}
+	}
+
+	return ret
+}
+
+type problem struct {
+	q string
+	a string
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
+}
